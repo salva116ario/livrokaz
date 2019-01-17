ce5077f5685364197d9868a67295c0c23f420aa9
salva116ario <salvatore.sancesario.lp2@gmail.com>
2019-01-17 12:41:42 +0100

    Style Controller fait chier !

--- src/main/java/co/simplon/livrokaz/controller/StyleController.java
+++ src/main/java/co/simplon/livrokaz/controller/StyleController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package co.simplon.livrokaz.controller;
 
 import java.util.List;
@@ -25,28 +24,28 @@ import co.simplon.livrokaz.repository.StyleRepository;
 @RequestMapping("/style")
 public class StyleController {
 
-	
-	
 	@Autowired
 	private StyleRepository styleRepository;
-	
+
 	/**
-	 * Retourne toutes les catégories de livres 
+	 * Retourne toutes les catégorie de livres
+	 * 
 	 * @return
 	 */
 	@GetMapping("/getall")
-	public ResponseEntity<?> getAllStyles(){
+	public ResponseEntity<?> getAllStyles() {
 		List<Style> styleList = null;
 		try {
 			styleList = styleRepository.findAll();
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}	
+		}
 		return ResponseEntity.status(HttpStatus.OK).body(styleList);
 	}
-	
+
 	/**
-	 * Retourne la catégorie avec id = {id}
+	 * Retourne la catégorie de livre avec id = {id}
+	 * 
 	 * @param id
 	 * @return
 	 */
@@ -60,8 +59,10 @@ public class StyleController {
 		}
 		return ResponseEntity.status(HttpStatus.OK).body(style);
 	}
+
 	/**
-	 * Ajoute une catégorie de livre
+	 * Ajoute une catégorie de livres
+	 * 
 	 * @param style
 	 * @return
 	 */
@@ -79,6 +80,7 @@ public class StyleController {
 
 	/**
 	 * Modifie une catégorie de livres
+	 * 
 	 * @param style
 	 * @return
 	 */
@@ -93,9 +95,10 @@ public class StyleController {
 		}
 		return ResponseEntity.status(HttpStatus.CREATED).body(styleToModify);
 	}
-	
+
 	/**
-	 * Supprime une catégorie de livres
+	 * Supprime la catégorie de livres avec id = {id}
+	 * 
 	 * @param id
 	 * @return
 	 */
@@ -110,93 +113,3 @@ public class StyleController {
 		return ResponseEntity.status(HttpStatus.OK).body(null);
 	}
 }
-=======
-package co.simplon.livrokaz.controller;
-
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import co.simplon.livrokaz.model.Style;
-import co.simplon.livrokaz.repository.StyleRepository;
-
-@RestController
-@RequestMapping("/style")
-public class StyleController {
-
-	
-	
-	@Autowired
-	private StyleRepository styleRepository;
-	
-	@GetMapping("/getall")
-	public ResponseEntity<?> getAllStyles(){
-		List<Style> styleList = null;
-		try {
-			styleList = styleRepository.findAll();
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}	
-		return ResponseEntity.status(HttpStatus.OK).body(styleList);
-	}
-	
-	@GetMapping("/getbyid/{id}")
-	public ResponseEntity<?> livreById(@PathVariable Integer id) {
-		Optional<Style> style = null;
-		try {
-			style = styleRepository.findById(id);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(style);
-	}
-	
-	@PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<?> addStyle(@RequestBody Style style) {
-		Style styleToAdd = null;
-		try {
-			styleToAdd = styleRepository.saveAndFlush(style);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
-		return ResponseEntity.status(HttpStatus.CREATED).body(styleToAdd);
-	}
-
-	@PutMapping(value = "/modify", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	@ResponseBody
-	public ResponseEntity<?> modifyStyle(@RequestBody Style style) {
-		Style styleToModify = null;
-		try {
-			styleToModify = styleRepository.saveAndFlush(style);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-		}
-		return ResponseEntity.status(HttpStatus.CREATED).body(styleToModify);
-	}
-	
-	@DeleteMapping(value = "/delete/{id}")
-	public ResponseEntity<?> deleteStyle(@PathVariable Integer id) {
-		try {
-			styleRepository.deleteById(id);
-		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-		}
-
-		return ResponseEntity.status(HttpStatus.OK).body(null);
-	}
-}
->>>>>>> branch 'master' of https://github.com/salva116ario/livrokaz.git
