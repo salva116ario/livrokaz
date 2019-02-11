66024d087ceb3c671efec91d7785ffad049a93b0
salva116ario <salvatore.sancesario.lp2@gmail.com>
2019-02-11 12:11:59 +0100

    UserRepo

--- src/main/java/co/simplon/livrokaz/repository/UserRepository.java
+++ src/main/java/co/simplon/livrokaz/repository/UserRepository.java
@@ -2,11 +2,11 @@ package co.simplon.livrokaz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import co.simplon.livrokaz.model.User0ld;
+import co.simplon.livrokaz.model.User;
 
 
-public interface UserRepository extends JpaRepository<User0ld, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
-    User0ld findByUsername(String username);
-    User0ld getById(Long id);
+    User findByUsername(String username);
+    User getById(Long id);
 }
